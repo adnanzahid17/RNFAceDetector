46c4a8de244964b8e61b5a91311511b50298b8e0
Adnan Zahid <adnanzahid17@gmail.com>
2025-10-29 19:40:59 +0500

    front camera is now visible

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,18 @@
 import { useEffect } from "react";
-import { Text, View } from "react-native";
-import { useCameraPermission } from "react-native-vision-camera";
+import { StyleSheet } from "react-native";
+import { Camera, useCameraDevice, useCameraPermission } from "react-native-vision-camera";
 export default function Index() {
   const { hasPermission, requestPermission } = useCameraPermission()
   // ask for camera permission
   useEffect(() => {
     if (!hasPermission) requestPermission();
   }, [hasPermission, requestPermission]);
+  const device = useCameraDevice('front')
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+    />
   );
 }
