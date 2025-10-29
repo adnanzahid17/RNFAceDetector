f242bde347e07f98658130b4ec5ddc87328a65b2
Adnan Zahid <adnanzahid17@gmail.com>
2025-10-29 19:49:28 +0500

    camera initial setup and getting frames

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import { StyleSheet } from "react-native";
-import { Camera, useCameraDevice, useCameraPermission } from "react-native-vision-camera";
+import { Dimensions, StyleSheet } from "react-native";
+import { Camera, useCameraDevice, useCameraFormat, useCameraPermission, useFrameProcessor } from "react-native-vision-camera";
 export default function Index() {
   const { hasPermission, requestPermission } = useCameraPermission()
   // ask for camera permission
@@ -8,11 +8,31 @@ export default function Index() {
     if (!hasPermission) requestPermission();
   }, [hasPermission, requestPermission]);
   const device = useCameraDevice('front')
+  const format = useCameraFormat(device, [
+    {
+      videoResolution: Dimensions.get('window'),
+    },
+    {
+      fps: 60,
+    },
+  ]);
+  const frameProcessor = useFrameProcessor((frame) => {
+    'worklet'
+
+    console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`)
+  }, [])
+
   return (
     <Camera
       style={StyleSheet.absoluteFill}
       device={device}
       isActive={true}
-    />
+      frameProcessor={frameProcessor}
+      enableFpsGraph
+      enableDepthData
+      videoStabilizationMode='auto'
+      photoQualityBalance='quality'
+      format={format}
+      fps={format?.maxFps} />
   );
 }
