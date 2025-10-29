c6e83463903df2654a14e02904c2da9b307d9b2f
Adnan Zahid <adnanzahid17@gmail.com>
2025-10-29 22:06:45 +0500

    a working face detector code

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,438 @@
-import { useEffect } from "react";
-import { Dimensions, StyleSheet } from "react-native";
-import { Camera, useCameraDevice, useCameraFormat, useCameraPermission, useFrameProcessor } from "react-native-vision-camera";
-export default function Index() {
-  const { hasPermission, requestPermission } = useCameraPermission()
-  // ask for camera permission
-  useEffect(() => {
-    if (!hasPermission) requestPermission();
-  }, [hasPermission, requestPermission]);
-  const device = useCameraDevice('front')
-  const format = useCameraFormat(device, [
-    {
-      videoResolution: Dimensions.get('window'),
-    },
-    {
-      fps: 60,
-    },
-  ]);
-  const frameProcessor = useFrameProcessor((frame) => {
-    'worklet'
+import { useAppState } from '@react-native-community/hooks'
+import { useIsFocused } from '@react-navigation/core'
+import {
+  ClipOp,
+  Skia,
+  TileMode
+} from '@shopify/react-native-skia'
+import React, {
+  ReactNode,
+  useEffect,
+  useRef,
+  useState
+} from 'react'
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions
+} from 'react-native'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from 'react-native-reanimated'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import {
+  CameraPosition,
+  DrawableFrame,
+  Frame,
+  Camera as VisionCamera,
+  useCameraDevice,
+  useCameraPermission
+} from 'react-native-vision-camera'
+import {
+  Camera,
+  Contours,
+  Face,
+  FaceDetectionOptions,
+  Landmarks
+} from 'react-native-vision-camera-face-detector'
 
-    console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`)
+/**
+ * Entry point component
+ *
+ * @return {ReactNode} Component
+ */
+function Index(): ReactNode {
+  return (
+    <SafeAreaProvider>
+
+      <FaceDetection />
+
+    </SafeAreaProvider>
+  )
+}
+
+/**
+ * Face detection component
+ *
+ * @return {ReactNode} Component
+ */
+function FaceDetection(): ReactNode {
+  const {
+    width,
+    height
+  } = useWindowDimensions()
+  const {
+    hasPermission,
+    requestPermission
+  } = useCameraPermission()
+  const [
+    cameraMounted,
+    setCameraMounted
+  ] = useState<boolean>(false)
+  const [
+    cameraPaused,
+    setCameraPaused
+  ] = useState<boolean>(false)
+  const [
+    autoMode,
+    setAutoMode
+  ] = useState<boolean>(true)
+  const [
+    cameraFacing,
+    setCameraFacing
+  ] = useState<CameraPosition>('front')
+  const faceDetectionOptions = useRef<FaceDetectionOptions>({
+    performanceMode: 'fast',
+    classificationMode: 'all',
+    contourMode: 'all',
+    landmarkMode: 'all',
+    windowWidth: width,
+    windowHeight: height
+  }).current
+  const isFocused = useIsFocused()
+  const appState = useAppState()
+  const isCameraActive = (
+    !cameraPaused &&
+    isFocused &&
+    appState === 'active'
+  )
+  const cameraDevice = useCameraDevice(cameraFacing)
+  //
+  // vision camera ref
+  //
+  const camera = useRef<VisionCamera>(null)
+  //
+  // face rectangle position
+  //
+  const aFaceW = useSharedValue(0)
+  const aFaceH = useSharedValue(0)
+  const aFaceX = useSharedValue(0)
+  const aFaceY = useSharedValue(0)
+  const aRot = useSharedValue(0)
+  const boundingBoxStyle = useAnimatedStyle(() => ({
+    position: 'absolute',
+    borderWidth: 4,
+    borderLeftColor: 'rgba(30, 0, 255, 1)',
+    borderRightColor: 'rgba(255, 234, 6, 1)',
+    borderBottomColor: 'rgb(0,255,0)',
+    borderTopColor: 'rgb(255,0,0)',
+    width: withTiming(aFaceW.value, {
+      duration: 100
+    }),
+    height: withTiming(aFaceH.value, {
+      duration: 100
+    }),
+    left: withTiming(aFaceX.value, {
+      duration: 100
+    }),
+    top: withTiming(aFaceY.value, {
+      duration: 100
+    }),
+    transform: [{
+      rotate: `${aRot.value}deg`
+    }]
+  }))
+
+  useEffect(() => {
+    if (hasPermission) return
+    requestPermission()
   }, [])
 
-  return (
-    <Camera
-      style={StyleSheet.absoluteFill}
-      device={device}
-      isActive={true}
-      frameProcessor={frameProcessor}
-      enableFpsGraph
-      enableDepthData
-      videoStabilizationMode='auto'
-      photoQualityBalance='quality'
-      format={format}
-      fps={format?.maxFps} />
-  );
+  /**
+   * Handle camera UI rotation
+   * 
+   * @param {number} rotation Camera rotation
+   */
+  function handleUiRotation(
+    rotation: number
+  ) {
+    aRot.value = rotation
+  }
+
+  /**
+   * Hanldes camera mount error event
+   *
+   * @param {any} error Error event
+   */
+  function handleCameraMountError(
+    error: any
+  ) {
+    console.error('camera mount error', error)
+  }
+
+  /**
+   * Handle detection result
+   * 
+   * @param {Face[]} faces Detection result 
+   * @param {Frame} frame Current frame
+   * @returns {void}
+   */
+  function handleFacesDetected(
+    faces: Face[],
+    frame: Frame
+  ): void {
+    // if no faces are detected we do nothing
+    if (faces.length <= 0) {
+      aFaceW.value = 0
+      aFaceH.value = 0
+      aFaceX.value = 0
+      aFaceY.value = 0
+      return
+    }
+
+    // console.log(
+    //   'faces', faces.length,
+    //   'frame', frame.toString(),
+    //   'faces', JSON.stringify(faces)
+    // )
+
+    const { bounds } = faces[0]
+    const {
+      width,
+      height,
+      x,
+      y
+    } = bounds
+    aFaceW.value = width
+    aFaceH.value = height
+    aFaceX.value = x
+    aFaceY.value = y
+
+    // only call camera methods if ref is defined
+    if (camera.current) {
+      // take photo, capture video, etc...
+    }
+  }
+
+  /**
+   * Handle skia frame actions
+   * 
+   * @param {Face[]} faces Detection result 
+   * @param {DrawableFrame} frame Current frame
+   * @returns {void}
+   */
+  function handleSkiaActions(
+    faces: Face[],
+    frame: DrawableFrame
+  ): void {
+    'worklet'
+    // if no faces are detected we do nothing
+    if (faces.length <= 0) return
+
+    console.log(
+      'SKIA - faces', faces.length,
+      'frame', frame.toString()
+    )
+
+    const {
+      bounds,
+      contours,
+      landmarks
+    } = faces[0]
+
+    // draw a blur shape around the face points
+    const blurRadius = 15
+    const blurFilter = Skia.ImageFilter.MakeBlur(
+      blurRadius,
+      blurRadius,
+      TileMode.Repeat,
+      null
+    )
+    const blurPaint = Skia.Paint()
+    blurPaint.setImageFilter(blurFilter)
+    const contourPath = Skia.Path.Make()
+    const necessaryContours: (keyof Contours)[] = [
+      'FACE',
+      'LEFT_CHEEK',
+      'RIGHT_CHEEK'
+    ]
+
+    necessaryContours.map((key) => {
+      contours?.[key]?.map((point, index) => {
+        if (index === 0) {
+          // it's a starting point
+          contourPath.moveTo(point.x, point.y)
+        } else {
+          // it's a continuation
+          contourPath.lineTo(point.x, point.y)
+        }
+      })
+      contourPath.close()
+    })
+
+    frame.save()
+    frame.clipPath(contourPath, ClipOp.Intersect, true)
+    frame.render(blurPaint)
+    frame.restore()
+
+    // draw mouth shape
+    const mouthPath = Skia.Path.Make()
+    const mouthPaint = Skia.Paint()
+    mouthPaint.setColor(Skia.Color('red'))
+    const necessaryLandmarks: (keyof Landmarks)[] = [
+      'MOUTH_BOTTOM',
+      'MOUTH_LEFT',
+      'MOUTH_RIGHT'
+    ]
+
+    necessaryLandmarks.map((key, index) => {
+      const point = landmarks?.[key]
+      if (!point) return
+
+      if (index === 0) {
+        // it's a starting point
+        mouthPath.moveTo(point.x, point.y)
+      } else {
+        // it's a continuation
+        mouthPath.lineTo(point.x, point.y)
+      }
+    })
+    mouthPath.close()
+    frame.drawPath(mouthPath, mouthPaint)
+
+    // draw a rectangle around the face
+    const rectPaint = Skia.Paint()
+    rectPaint.setColor(Skia.Color('blue'))
+    rectPaint.setStyle(1)
+    rectPaint.setStrokeWidth(5)
+    frame.drawRect(bounds, rectPaint)
+  }
+
+
+
+  return (<>
+    <View
+      style={[
+        StyleSheet.absoluteFill, {
+          alignItems: 'center',
+          justifyContent: 'center'
+        }
+      ]}
+    >
+      {hasPermission && cameraDevice ? <>
+        {cameraMounted && <>
+          <Camera
+            // @ts-ignore
+            ref={camera}
+            style={StyleSheet.absoluteFill}
+            isActive={isCameraActive}
+            device={cameraDevice}
+            onError={handleCameraMountError}
+            faceDetectionCallback={handleFacesDetected}
+            onUIRotationChanged={handleUiRotation}
+            // @ts-ignore
+            skiaActions={handleSkiaActions}
+            faceDetectionOptions={{
+              ...faceDetectionOptions,
+              autoMode,
+              cameraFacing
+            }}
+          />
+
+          <Animated.View
+            style={boundingBoxStyle}
+          />
+
+          {cameraPaused && <Text
+            style={{
+              width: '100%',
+              backgroundColor: 'rgb(0,0,255)',
+              textAlign: 'center',
+              color: 'white'
+            }}
+          >
+            Camera is PAUSED
+          </Text>}
+        </>}
+
+        {!cameraMounted && <Text
+          style={{
+            width: '100%',
+            backgroundColor: 'rgb(255,255,0)',
+            textAlign: 'center'
+          }}
+        >
+          Camera is NOT mounted
+        </Text>}
+      </> : <Text
+        style={{
+          width: '100%',
+          backgroundColor: 'rgb(255,0,0)',
+          textAlign: 'center',
+          color: 'white'
+        }}
+      >
+        No camera device or permission
+      </Text>}
+    </View>
+
+    <View
+      style={{
+        position: 'absolute',
+        bottom: 20,
+        left: 0,
+        right: 0,
+        display: 'flex',
+        flexDirection: 'column'
+      }}
+    >
+      <View
+        style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around'
+        }}
+      >
+
+
+
+      </View>
+
+      <View
+        style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around'
+        }}
+      >
+        <Button
+          onPress={() => setCameraFacing((current) => (
+            current === 'front' ? 'back' : 'front'
+          ))}
+          title={'Toggle Cam'}
+        />
+
+        <Button
+          onPress={() => setAutoMode((current) => !current)}
+          title={`${autoMode ? 'Disable' : 'Enable'} AutoMode`}
+        />
+      </View>
+      <View
+        style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around'
+        }}
+      >
+        <Button
+          onPress={() => setCameraPaused((current) => !current)}
+          title={`${cameraPaused ? 'Resume' : 'Pause'} Cam`}
+        />
+
+        <Button
+          onPress={() => setCameraMounted((current) => !current)}
+          title={`${cameraMounted ? 'Unmount' : 'Mount'} Cam`}
+        />
+      </View>
+    </View>
+  </>)
 }
+
+export default Index
